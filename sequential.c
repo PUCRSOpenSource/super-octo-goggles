bed444d7847de5638431cc4e574ec741256b8a18
Matthias Nunes <matthiasnunes@gmail.com>
2016-04-08 23:08:06 -0300

    Declares matrix and initializes it

--- sequential.c
+++ sequential.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define ROWS 1000
+#define COLUMNS 100000
+
+int vet[ROWS][COLUMNS];
 
 int main(int argc, const char* argv[])
 {
+	int i, j, k = COLUMNS;
+	for (i = 0; i < ROWS; i++)
+	{
+		for (j = 0; j < COLUMNS; j++)
+		{
+			vet[i][j] = k;
+			k--;
+		}
+		k = COLUMNS;
+	}
 	return 0;
 }
